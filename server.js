05cbef302b217f261e28b98703e784ba390b207b
Kallol Chatterjee (Student) <kchatte1@sundevils.asu.edu>
2016-10-20 17:17:03 -0700

    Sentiment analysis chart

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var http = require('http');
+var  ejs = require('ejs');
+var bodyParser = require('body-parser');
+var elasticsearch = require('elasticsearch');
+var app = express();
+var client = new elasticsearch.Client({
+  host: 'http://55be81dc.ngrok.io/'
+});
+app.set('views', './views');
+app.engine('html', ejs.renderFile);
+app.use(express.static('./js'));
+app.use(express.static('./images'));
+app.use(express.static('./styles'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+http.createServer(app).listen(80);
+app.get('/', function(req, res) {
+    var neutralValue;
+    var negativeValue;
+    var positiveValue;
+      client.search({
+          index: 'project_1',
+          type: 'tweet',
+          body: {
+            size: 0,
+            query: {
+              match_all: {}
+            },
+            "aggregations":
+            {
+                "aggs": {
+                    "terms": {
+                      "field": "sentiments"
+                    }
+                }
+            }
+          }
+      }).then(function (resp) {
+          // console.log(resp);
+          neutralValue = resp.aggregations.aggs.buckets[0].doc_count;
+          negativeValue = resp.aggregations.aggs.buckets[2].doc_count;
+          positiveValue = resp.aggregations.aggs.buckets[1].doc_count;
+          // console.log(neutralValue);
+          // console.log(negativeValue);
+          // console.log(positiveValue);
+          var sentimentDataArray = [];
+          var title = ["sentiment", "value"];
+          sentimentDataArray.push(title);
+          var positiveObj = ["Positive", positiveValue];
+          sentimentDataArray.push(positiveObj);
+          var neutralObj = ["Neutral", neutralValue];
+          sentimentDataArray.push(neutralObj);
+          var negativeObj = ["Negative", negativeValue];
+          sentimentDataArray.push(negativeObj);
+          app.render('index.html', { sentimentData: JSON.stringify(sentimentDataArray) },function(err, renderedData) {
+             res.send(renderedData);
+         });
+          }, function (err) {
+              console.trace(err.message);
+      });
+});
